df0d0811482ee6cb9a0f8c482f1e950dc29c0a53
Amir Elemam <amirelemam@email.com>
2016-09-14 23:23:16 -0300

    Código 28-08

---
+++ Monitoria_IPOO/2016-08-28/Código/Soma.java
@@ -0,0 +1,16 @@
+public class Soma
+{
+	private int num1;
+	private int num2;
+
+	public Soma()
+	{
+		num1 = 0;
+		num2 = 0;
+	}	
+	public Soma int(int num1, int num2)
+	{
+		this.num1 = num1;
+		this.num2 = num2;
+	}
+}
